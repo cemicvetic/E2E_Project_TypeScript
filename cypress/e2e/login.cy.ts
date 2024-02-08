f43bac7808de231236883ad84817ff9cb174a7d3
cemicvetic <cemicvetic@gmail.com>
2024-02-07 16:44:45 -0800

    Add POM class

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,12 +1,19 @@
+import{LoginPage} from "../../pages/Login";
+
 describe('LOGIN PAGE', () => {
     beforeEach(() => {
         cy.visit(`${Cypress.env('demoQA')}/login`)
         cy.intercept({resourceType: /xhr|fetch/}, {log: false})
     })
     it('login', () => {
-        cy.get('input[placeholder=\'UserName\']').type('test')
+        cy.get('#userName').type('test')
         cy.get('#password').type('Test1234*')
         cy.get('#login').click()
         cy.contains('Log out')
     })
+    it('login with PageObject', () => {
+        LoginPage.submitButtonLogin()
+        cy.contains('Log out')
+    })
+
 })
