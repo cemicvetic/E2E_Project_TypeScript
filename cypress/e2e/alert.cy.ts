dfe40186c23c36a794a18446776e00bee684d63a
cemicvetic <cemicvetic@gmail.com>
2024-03-06 09:13:28 -0800

    Add class Alert.ts

--- cypress/e2e/alert.cy.ts
+++ cypress/e2e/alert.cy.ts
@@ -1,6 +1,14 @@
+import{ AlertPage} from '../../pages/Alert'
+
 describe('ALERT', () => {
     beforeEach(() => {
         cy.visit(`${Cypress.env('herokuapp')}/javascript_alerts`)
     })
-    it('', () => {})
+    it('Click for JS Alert', () => {
+        AlertPage.jsAlert()
+
+
+
+    })
 })
+
