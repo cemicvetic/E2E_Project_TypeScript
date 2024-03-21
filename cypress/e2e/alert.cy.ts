77064456a1557056297157dc53aaa0d9106a58db
cemicvetic <cemicvetic@gmail.com>
2024-03-20 20:58:56 -0700

    Add tests Alert.ts

--- cypress/e2e/alert.cy.ts
+++ cypress/e2e/alert.cy.ts
@@ -7,4 +7,21 @@ describe('ALERT', () => {
     it('Click for JS Alert', () => {
         AlertPage.jsAlert()
     })
+
+    it('Click for JS Confirm OK', () => {
+        AlertPage.jsConfirm()
+    })
+    it('Click for JS Confirm False', () => {
+        //homework
+    })
+    it('Click for JS Prompt OK/true', () => {
+        AlertPage.jsPrompt()
+    })
+    it.skip('Click for JS Prompt OK/true', () => {
+        cy.window().then(($win) => {
+            cy.stub($win, 'prompt').returns('Hello Hello')
+            cy.contains('button', 'Click for JS Prompt').click()
+        })
+        cy.get('#result').should('have.text', 'You entered: Hello Hello')
+    })
 })
--- pages/Alert.ts
+++ pages/Alert.ts
@@ -1,13 +1,45 @@
 class Alert {
     private alertTextButton: string = 'Click for JS Alert'
     private buttonSelector: string = 'button'
+    //private buttonPromptText: string = 'Click on JS Prompt'
+    //popWindowText
+    private jsAlertText: string = 'I am a JS Alert'
+    private jsConfirmWindowText: string = 'I am a JS Confirm'
+
+    private resultSelector: string = '#result'
+    private resultTextJSAlert: string = 'You successfully clicked an alert'
+    private resultJsConfirmText: string = 'You clicked: Ok'
+    private jsConfirmText: string = 'Click for JS Confirm'
+    private jsPromptText: string = 'Click for JS Prompt'
+
     jsAlert() {
         cy.contains(this.buttonSelector, this.alertTextButton).click()
-        cy.on('window:alert', (text) => {
-            expect(text).to.equal('I am a JS Alert')
+        cy.on('window:alert', (textAlert) => {
+            expect(textAlert).to.equal(this.jsAlertText)
         })
         cy.on('window:confirm', () => true)
-        cy.get('#result')
+        cy.get(this.resultSelector).should('have.text', this.resultTextJSAlert)
+    }
+
+    jsConfirm() {
+        cy.contains(this.buttonSelector, this.jsConfirmText).click()
+        cy.on('window:alert', (textAlert) => {
+            expect(textAlert).to.equal(this.jsConfirmWindowText)
+        })
+        cy.on('window:confirm', () => true)
+        cy.get(this.resultSelector).should(
+            'have.text',
+            this.resultJsConfirmText
+        )
+    }
+
+    jsPrompt() {
+        const text = 'Hello Hello'
+        cy.window().then(($win) => {
+            cy.stub($win, 'prompt').returns(text)
+            cy.contains(this.buttonSelector, this.jsPromptText).click()
+        })
+        cy.get(this.resultSelector).should('include.text', text)
     }
 }
 export const AlertPage = new Alert()
