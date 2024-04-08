b96f52b9d984027202dce34a919aeef8b202a26c
cemicvetic <cemicvetic@gmail.com>
2024-04-08 15:37:24 -0700

    Add tests brokenImage.cy.ts

--- cypress/e2e/brokenImage.cy.ts
+++ cypress/e2e/brokenImage.cy.ts
@@ -13,4 +13,14 @@ describe('BROKEN IMAGE', () => {
                 expect(img.naturalWidth, `Image naturalWidth size is ${img.naturalWidth}`).to.eq(0)
             })
     })
+    it('Verify that image is not broken', () => {
+        cy.get('div.example img')
+            .last()
+            .should('be.visible')
+            .and((el) => {
+                img = el[0] as HTMLImageElement
+                expect(img.naturalHeight, `Image naturalHeight size is ${img.naturalHeight}`).to.eq(160)
+                expect(img.naturalWidth, `Image naturalWidth size is ${img.naturalWidth}`).to.eq(160)
+            })
+    })
 })
