c846cc98139eab6bd93a4686ba5ca7e2c6720d48
cemicvetic <cemicvetic@gmail.com>
2024-04-18 12:10:34 -0700

    Add reporter, run test

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,5 @@
 import {defineConfig} from 'cypress';
 
-const xlsx = require("node-xlsx").default;
-const fs = require("fs"); // for file
-const path = require("path")
-
-require('dotenv').config()
 export default defineConfig({
     reporter: 'cypress-mochawesome-reporter',
     e2e: {
@@ -15,18 +10,7 @@ export default defineConfig({
             require('cypress-mochawesome-reporter/plugin')(on);
             // implement node event listeners here
             //reading excel document from fixture
-            on("task", {
-                parseXlsx({ filePath }) {
-                    return new Promise((resolve, reject) => {
-                        try {
-                            const jsonData = xlsx.parse(fs.readFileSync(filePath));
-                            resolve(jsonData);
-                        } catch (e) {
-                            reject(e);
-                        }
-                    });
-                },
-            });
+
         },
         env: {
             stage: 'https://stage.pasv.us',
